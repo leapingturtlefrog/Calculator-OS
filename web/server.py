96902a95aea0c88ce58eb0bc9ea5b9bb9057f7f3
Alex Aridgides <alexanderaridgides@gmail.com>
2026-02-05 16:44:49 -0800

    update paths

--- web/server.py
+++ web/server.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 import http.server
 import sys
+import urllib.parse
 
 class H(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        parsed = urllib.parse.urlsplit(self.path)
+        path = parsed.path
+        if path == '/calculator-os' or path == '/calculator-os/':
+            path = '/index.html'
+        elif path.startswith('/calculator-os/'):
+            path = '/' + path[len('/calculator-os/'):]
+        if path != parsed.path:
+            query = f'?{parsed.query}' if parsed.query else ''
+            self.path = f'{path}{query}'
+        return super().do_GET()
+
     def end_headers(self):
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
